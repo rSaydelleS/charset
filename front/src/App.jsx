2a35da36117fe6250ded0c958e9544bf892d5424
rSaydelleS <rafael.ppuerari@gmail.com>
2024-08-15 19:21:06 -0300

    feat: criando a header e modificando o css padrao

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import Header from "./components/Header/header";
 
 function App() {
   return (
     <>
-      <section> Hellow World</section>
+      <Header />
     </>
   );
 }
---
+++ front/src/components/Header/header.jsx
@@ -0,0 +1,15 @@
+export default function Header() {
+  return (
+    <>
+      <main>
+        <header>
+          <section>nome do personagem</section>
+          <section>
+            <div>status</div>
+            <div>status</div>
+          </section>
+        </header>
+      </main>
+    </>
+  );
+}
