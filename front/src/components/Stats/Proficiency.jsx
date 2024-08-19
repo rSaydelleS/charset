811fdc9464ed2b2087f30d808011186411868985
rSaydelleS <rafael.ppuerari@gmail.com>
2024-08-19 19:05:29 -0300

    style: estilizando as salva guardas

--- front/src/components/Stats/Proficiency.jsx
+++ front/src/components/Stats/Proficiency.jsx
@@ -5,45 +5,47 @@ export default function Proficiency() {
     <>
       <main>
         <section>
-          <div className="proficiency">
-            <input type="text" />
-            <label>Bonus de proficiência</label>
-          </div>
-          <div className="inspiration">
-            <input type="text" />
-            <label>Inspirações</label>
-          </div>
+          <section className="proficiency">
+            <div>
+              <input type="text" />
+              <label>Bonus de proficiência</label>
+            </div>
+            <div className="inspiration">
+              <input type="text" />
+              <label>Inspirações</label>
+            </div>
+          </section>
           <div className="sv">
             <div>
               <ul>
                 <li>
                   <input type="checkbox" />
-                  <input type="number" />
+                  <input type="text" />
                   <label>Força</label>
                 </li>
                 <li>
                   <input type="checkbox" />
-                  <input type="number" />
+                  <input type="text" />
                   <label>Constituição</label>
                 </li>
                 <li>
                   <input type="checkbox" />
-                  <input type="number" />
+                  <input type="text" />
                   <label>Destreza</label>
                 </li>
                 <li>
                   <input type="checkbox" />
-                  <input type="number" />
+                  <input type="text" />
                   <label>Inteligência</label>
                 </li>
                 <li>
                   <input type="checkbox" />
-                  <input type="number" />
+                  <input type="text" />
                   <label>Sabedoria</label>
                 </li>
                 <li>
                   <input type="checkbox" />
-                  <input type="number" />
+                  <input type="text" />
                   <label>Carisma</label>
                 </li>
               </ul>
