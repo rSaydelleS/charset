c4739e8cb2a970e121f0305ff71f9e42b4f74a95
rSaydelleS <rafael.ppuerari@gmail.com>
2024-08-22 19:53:46 -0300

    feat: criando as rotas CRUD

--- back/src/routes/users.js
+++ back/src/routes/users.js
@@ -7,4 +7,31 @@ routes.get("/", (req, res) => {
   });
 });
 
+routes.get("/:id", (req, res) => {
+  const id = req.params.id;
+  res.status(200).json({
+    message: `Trazendo user com id: ${id}`,
+  });
+});
+
+routes.put("/update/:id", (req, res) => {
+  const id = req.params.id;
+  res.status(200).json({
+    message: `Rota de atualizacao do usuario ${id}`,
+  });
+});
+
+routes.post("/create", (req, res) => {
+  res.status(200).json({
+    message: "Rota de criacao de usuario",
+  });
+});
+
+routes.delete("/delete/:id", (req, res) => {
+  const id = req.params.id;
+  res.status(200).json({
+    message: `Usuario de id ${id} deletado`,
+  });
+});
+
 module.exports = routes;
