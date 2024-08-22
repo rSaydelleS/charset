de227c4d90fda03cc4511d0b7830faaf2b51807b
rSaydelleS <rafael.ppuerari@gmail.com>
2024-08-22 19:37:50 -0300

    chore: configurando as rotas

--- back/index.js
+++ back/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 require("dotenv").config();
 const app = express();
-const port = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const routes = require("./src/routes/users");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -11,6 +12,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.listen(port, () => {
-  console.log(`Servidor operacional na porta: ${port}`);
+app.listen(PORT, () => {
+  console.log(`Servidor operacional na porta: ${PORT}`);
 });
+
+app.use("/users", routes);
